cfcbb3f5189b091c960d84732abd3771984a275a
Hitesh Goel <101472024+Hitesh-073@users.noreply.github.com>
2026-03-20 14:53:24 +0000

    Feat/schema1 (#4)
    * EcommerceAnalytics schema file created
    
    * Moved Schema file into correct directory

--- 01_schema_design/create_schema.sql
+++ 01_schema_design/create_schema.sql
@@ -1,1 +1,31 @@
-select * from dbo.emp
+CREATE DATABASE EcommerceAnalytics;
+GO
+
+USE EcommerceAnalytics;
+GO
+
+CREATE TABLE Customers
+(
+    CustomerID INT IDENTITY PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    CreatedDate DATETIME
+);
+
+CREATE TABLE Products
+(
+    ProductID INT IDENTITY PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Price DECIMAL(10,2)
+);
+
+CREATE TABLE Orders
+(
+    OrderID INT IDENTITY PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATETIME,
+    TotalAmount DECIMAL(10,2),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
